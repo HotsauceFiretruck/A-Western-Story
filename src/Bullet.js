ba9e066b52640ef3b49d8aef303f41bbc1293737
tranqnhan <nt305748@gmail.com>
2019-10-11 22:16:16 -0500

    changes and updates
    - Add basic enemy and player
    - Move Game into a class
    
    Current Problems:
    - Cannot rotate collision box
    
    Proposed Solution:
    - Remake the game into using matter engine rather than arcade engine.

--- src/Bullet.js
+++ src/Bullet.js
@@ -1,21 +1,47 @@
 export class Bullet extends Phaser.GameObjects.Sprite
 {
-    constructor(scene, x, y, vX, vY, target)
+    constructor(scene, target, fromX, fromY, toX, toY)
     {
-        super(scene, x, y, 'bullet');
+        super(scene, fromX, fromY, 'bullet');
+
+        //Add to Scene
         scene.add.existing(this);
-        scene.projectiles.add(this);
+        scene.projectileGroup.add(this);
+
+        //Set a speed value
+        let speed = 300;
+
+        //Normalize Vector X, Y and Multiply by Speed
+        let distX = toX - fromX;
+        let distY = toY - fromY;
+        let magnitude = Math.sqrt(distX * distX + distY * distY);
+        let normalizeX = 0;
+        let normalizeY = 0;
+
+        if (magnitude > 0)
+        {
+            normalizeX = distX / magnitude;
+            normalizeY = distY / magnitude;
+        } else 
+        {
+            normalizeX = 1;
+        }
+
+        let vX = normalizeX * speed;
+        let vY = normalizeY * speed;
+        let radians = Math.atan2(vY, vX);
+
+        //Settings
         scene.physics.world.enableBody(this);
+        scene.physics.add.collider(scene.platformGroup, this, this.destroy, null, this);
         this.body.setAllowGravity(false);
-        this.body.velocity.x = vX;
-        this.body.velocity.y = vY;
+        this.body.setVelocity(vX, vY);
+        this.setRotation(radians);
+        this.body.allowRotation = true;
+        this.body.rotation = radians; 
 
-
-        //Uncomment this and commment the bottom one to get the deflecting bullet effect
-        //scene.physics.add.collider(this, scene.platforms);
-        //scene.physics.add.collider(this, scene.player);
         if (target != null) {
-            scene.physics.add.collider(target, this, this.hit, null, this); //bottom one
+            scene.physics.add.collider(target, this, this.hit, null, this);
         }
     } 
       
@@ -33,6 +59,4 @@ export class Bullet extends Phaser.GameObjects.Sprite
             this.destroy();
         }
     }
-
-
 }
