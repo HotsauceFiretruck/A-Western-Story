980de4d95c69376ce69be363fbbd7c409e05620a
tranqnhan <nt305748@gmail.com>
2019-10-15 22:38:10 -0500

    Tile Mapping should be good
    I hope that there is not going to be any bugs, which there probably will be.

--- src/levels/TileMap.js
+++ src/levels/TileMap.js
@@ -43,22 +43,25 @@ export class TileMap
         //Check across
         // console.log(currentWidth);
         let currentWidth = this.searchWide(fromTileX, fromTileY, -1);
-        //console.log(fromTileX + currentWidth);
+        // console.log("CW: " + currentWidth);
         //Check below
-        let debugTimeRun = 0;
+        // var debugTimeRun = 0;
         let currentLength = 1;
         for (let y = fromTileY + 1; y < this.level.length; y++)
         {   
-            debugTimeRun++;
-            console.log(y + " " + debugTimeRun);
-            if (this.searchWide(fromTileX, y, currentWidth) != currentWidth)
+            // debugTimeRun++;
+            // console.log("frTY: " + fromTileY + "; tll: " + this.level.length);
+            // console.log("y: " + y + "; debugTime " + debugTimeRun + "; cw: " + currentWidth);
+            if (this.searchWide(fromTileX, y, currentWidth) == currentWidth)
             {
-                currentLength = y - fromY + 1;
-                console.log("cL: " + currentLength);
-                return;
+                currentLength++;
+                // console.log("CL: " + currentLength);
+            } else 
+            {
+                break;
             }
         }
-        console.log(debugTimeRun);
+        // console.log("Out - debugTime: " + debugTimeRun);
         this.generateBounds(fromTileX, fromTileY, fromTileX + currentWidth - 1, fromTileY + currentLength - 1);
     }
 
@@ -70,30 +73,38 @@ export class TileMap
         {
             if (this.level[atY][x] == 1 && !this.isArrayinArray(this.finishedLoadTile, [atY, x])) //If the tile is grass and not in finishedLoadTile
             {
-                console.log("-- searchWide: " + atY + ": " + x);
-                this.finishedLoadTile.push([atY, x]);
+                // console.log("-- searchWide: " + atY + ": " + x);
                 width += 1;
-                //console.log("WIDTH: " + width);
+                //console.log("STILL-ADD-WIDTH: " + width);
             }
             else // If the tile is not grass
             {
-                console.log("WIDTH: " + width);
-                return width;
+                //console.log("WIDTH-WHEN-NOT-GRASS: " + width);
+                break;
                 
             }
             if (width == optionalWidth)
             {
-                return width;
+                //console.log("WIDTH-EQUAL: " + width);
+                break;
             }
         }
 
-        console.log("WIDTH: " + width);
+        if (optionalWidth == -1 || width == optionalWidth)
+        {
+            for (let x = fromX; x < fromX + width; x++)
+            {
+                this.finishedLoadTile.push([atY, x]);
+            }
+        }
+
+        // console.log("WIDTH-OUTSIDE: " + width);
         return width;
     }
 
     generateBounds(fromTileX, fromTileY, toTileX, toTileY)
     {
-        console.log("Generating Bounds...");
+        //console.log("Generating Bounds...");
         let fromX = (fromTileX) *  this.tileWidth;
         let fromY = (fromTileY) * this.tileHeight;
         let toX = (toTileX + 1) * this.tileWidth;
@@ -102,10 +113,10 @@ export class TileMap
         let bodyHeight = toY - fromY;
         let centerX = fromX + (bodyWidth / 2);
         let centerY = fromY + (bodyHeight / 2);
-        console.log("ft x: " + fromTileX + "; ft y: " + fromTileY);
-        console.log("tt x: " + toTileX + "; tt y: " + toTileY);
-        console.log("from x: " + fromX + "; from y: " + fromY + "; toX: " + toX + "; toY: " + toY);
-        console.log("center x: " + centerX + "; center y: " + centerY);
+        // console.log("ft x: " + fromTileX + "; ft y: " + fromTileY);
+        // console.log("tt x: " + toTileX + "; tt y: " + toTileY);
+        // console.log("from x: " + fromX + "; from y: " + fromY + "; toX: " + toX + "; toY: " + toY);
+        // console.log("center x: " + centerX + "; center y: " + centerY);
         this.scene.matter.add.rectangle(centerX, centerY, bodyWidth, bodyHeight, { isStatic: true});
 
         for (let y = fromTileY; y <= toTileY; y++)
@@ -113,7 +124,7 @@ export class TileMap
             for (let x = fromTileX; x <= toTileX; x++)
             {
                 this.scene.add.image(this.tileWidth * (x + 1/2), this.tileHeight * (y + 1/2), this.imageKey);
-                console.log("y: " + y + "; x: " + x);
+                // console.log("y: " + y + "; x: " + x);
             }
         }
     }
