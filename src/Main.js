896a96c088c98c40ebbadade74777942743a259c
tranqnhan <nt305748@gmail.com>
2019-10-23 20:41:07 -0500

    device scaling

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,3 @@
 import { Game } from "./Game.js";
 
-// Phaser.D
-
-// if (Phaser.Device.OS.desktop)
-// {
-//     console.log("Desktop Detected!");
-// }
-// if (Phaser.Device.OS.iPhone)
-// {
-//     console.log("Iphone Detected!")
-// }
-
 let A_Western_Story = new Game();
