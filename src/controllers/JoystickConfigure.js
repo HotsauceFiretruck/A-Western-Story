8349878d35943476b91c45648c4648ab773880b3
tranqnhan <nt305748@gmail.com>
2019-10-29 18:51:41 -0500

    k

--- src/controllers/JoystickConfigure.js
+++ src/controllers/JoystickConfigure.js
@@ -8,35 +8,21 @@ export class Joystick
 
         this.centerX = x;
         this.centerY = y; //scene.cameras.main.height - 150;
-<<<<<<< HEAD
         this.rad = rad;
         this.pointer = pointer;
-=======
-        this.rad = rad * scale;
->>>>>>> cf99856c9134540fadba75b47479eeab7d50554b
 
         this.isThumbTouched = false;
         this.isBaseTouched = false;
 
         this.base = scene.add.image(0, 0, baseImageKey).setDisplaySize(size * scale, size * scale);
         this.base.setScrollFactor(0, 0).setInteractive();
-<<<<<<< HEAD
         this.base.on(pointer.isDown, () => { this.isBaseTouched = true});
         this.base.on(pointer.isUp, () => { this.isBaseTouched = false});;
-=======
-        this.base.on('pointerdown', () => {this.isBaseTouched = true});
-        //this.base.on('pointerup', () => {this.isBaseTouched = true});;
->>>>>>> cf99856c9134540fadba75b47479eeab7d50554b
 
         this.thumb = scene.add.image(0, 0, thumbImageKey).setDisplaySize((size / 2) * scale, (size / 2) * scale);
         this.thumb.setScrollFactor(0, 0).setInteractive();
-<<<<<<< HEAD
         this.thumb.on(pointer.isDown, () => { this.isThumbTouched = true});
         this.thumb.on(pointer.isUp, () => { this.isThumbTouched = false});
-=======
-        this.thumb.on('pointerdown', () => {this.isThumbTouched = true});
-        //this.thumb.on('pointerup', () => {this.isThumbTouched = true});
->>>>>>> cf99856c9134540fadba75b47479eeab7d50554b
 
         this.joystick = scene.rexVirtualJoyStick.add(scene, {
             x: this.centerX,
