0a683d64988f14ca3087ca3347c24154aebc6fe6
Alex Dawson <aad4wson@gmail.com>
2019-11-01 14:38:11 -0500

    doing my level

--- src/StoryMode.js
+++ src/StoryMode.js
@@ -71,7 +71,7 @@ export class StoryMode
                 ]
             },
           
-            scene: [levelTutorial, death]
+            scene: [level1, death]
         };
 
         let game = new Phaser.Game(this.config);
