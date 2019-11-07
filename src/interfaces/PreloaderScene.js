4a3953f8c3d9a340d09fc9b54083c5ea76e5597a
CodingSyntax <>
2019-11-07 17:22:05 -0600

    update level

--- src/interfaces/PreloaderScene.js
+++ src/interfaces/PreloaderScene.js
@@ -84,8 +84,11 @@ export class PreloaderScene extends Phaser.Scene
         }
         this.load.image('background', 'assets/Background.png');
         this.load.image('background2', 'assets/Background2.png');
+        this.load.image('background3', 'assets/Background3.png');
+        this.load.image('background4', 'assets/Background4.png');
         this.load.image('grass', 'assets/Grass.png');
         this.load.image('sand', 'assets/Sand.png');
+        this.load.image('clear', 'assets/Clear.png');
         this.load.image('player', 'assets/Player.png');
         this.load.image('bullet', 'assets/Bullet.png');
         this.load.image('house', 'assets/House.png');
@@ -93,6 +96,7 @@ export class PreloaderScene extends Phaser.Scene
         this.load.image('sun', 'assets/Sun1.png');
         this.load.image('cloud', 'assets/Cloud.png');
         this.load.image('sheriff', 'assets/Sheriff.png');
+        this.load.image('sheriffhouse', 'assets/SheriffHouse.png');
         this.load.audio('cartheftmusic', 'assets/CarTheft.mp3');
         this.load.image('cactus', 'assets/Cactus.png');
         this.load.image('crate', 'assets/Crate.png');
