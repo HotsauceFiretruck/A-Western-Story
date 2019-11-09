7dd31d50bdb55a7acb128aa89fd6910325210b11
ETRulz <34170237+ETRulz@users.noreply.github.com>
2019-11-08 23:18:07 -0600

    adding comments to my level

--- src/levels/EthanLevel.js
+++ src/levels/EthanLevel.js
@@ -17,6 +17,8 @@ export class EthanLevel extends Phaser.Scene
     create()
     {
         // Tile Map for start of level 4
+        // 0s symbolize empty spaces
+        // 1s symbolize ground spaces/squares/tiles or whatever you want to call them
         let level = 
         [   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -39,11 +41,15 @@ export class EthanLevel extends Phaser.Scene
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ];
 
+        // This .loopImage loops the background texture so that we don't have to add several and line them up perfectly
         this.loopImage('background2', 720, 420, level[0].length * 32, level.length * 32, 1.45);
+        // Adding images to the background.
+        // Usage - 
+        // scene.name = scene.add.image(x-pos, y-pos, 'imageName');
         this.cactus1 = this.add.image(800, 530, 'cactus');
         this.cactus2 = this.add.image(1100, 500, 'cactus');
         this.cactus3 = this.add.image(1725, 530, 'cactus');
-        this.bigcrate = this.add.image(700, 532, 'crate').setScale(.9);
+        this.bigcrate = this.add.image(700, 532, 'crate').setScale(.9);  // .setScale scales the image properly to fit the desired size.
         this.bigcrate2 = this.add.image(1550, 532, 'crate').setScale(.9);
         this.deadTree = this.add.image(370, 425, 'deadtree').setScale(1.75);
         this.deadTree2 = this.add.image(3000, 490, 'deadtree').setScale(1.75);
@@ -52,9 +58,10 @@ export class EthanLevel extends Phaser.Scene
         this.map = new TileMap(this, level, 32, 32, 'sand');
         this.player = new Player(this, 3000, 550);
 
+        // Sets the goal and function for this area
+        // When reached the the function .switchToChurch will run, causing things to change and update.
         this.nextLevelGoal = new Area(this, 'signPost', 3200, 525, 510, 400).setScale(.25);
         this.nextLevelGoal.whenTouched(this.player, () => {
-            // make it so there are multiple directions to choose from, or just make it rng whether or not player gets to see church area; if not just go to town and then to final level.
             this.switchToChurch()
         });
         
@@ -68,6 +75,9 @@ export class EthanLevel extends Phaser.Scene
             list: []
         };
         
+        // Spawning in enemies.
+        // Usage - 
+        // scene.enemyName = new EnemyType(scene, x-pos, y-pos)
         this.Snake1 = new Snake(this, 850, 575); 
         this.Snake2 = new Snake(this, 750, 575);
         this.Snake3 = new Snake(this, 1900, 575); 
@@ -76,6 +86,7 @@ export class EthanLevel extends Phaser.Scene
         this.basicEnemy1 = new Enemy(this, 1600, 550);
         this.basicEnemy2 = new Enemy(this, 2850, 575);
         
+        // This creates and displays the dialog boxes seen at the top of the screen at certain points in the level.
         let dialogTree = new DialogTree(this, 600, 100);
         let sequence0 = dialogTree.addSequence();
         let sequence1 = dialogTree.addSequence();
@@ -96,9 +107,10 @@ export class EthanLevel extends Phaser.Scene
 
     switchToChurch()
     {
+        // Deleting platforms on the map so as to not have invisible blocks when the level updates with new changes to the tilemap.
         this.map.deleteAllPlatforms();
 
-        // Removing images & enemies used in previous tile map
+        // Removing images, enemies, and nextLevelGoal used in previous tile map
         this.nextLevelGoal.destroy();
         this.cactus1.destroy();
         this.cactus2.destroy();
@@ -117,9 +129,6 @@ export class EthanLevel extends Phaser.Scene
         this.basicEnemy2.death();
 
         // Tile Map to 'switch scene' without making whole new level file. This map is for the town you can find.
-
-        // Placed some secret blocks to use to jump up to the special upgraded gun for player
-        // Easier to do this then deal with dialogue/puzzle conversation with priests.
         let levelChurch = 
         [   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -142,6 +151,9 @@ export class EthanLevel extends Phaser.Scene
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ];
 
+        // Adding images to the background.
+        // Usage - 
+        // scene.name = scene.add.image(x-pos, y-pos, 'imageName');
         this.church = this.add.image(1850, 317, 'church').setScale(2.25);
         this.deadTreeChurch = this.add.image(1550, 488, 'deadtree').setScale(1.75);
         this.bigcrateChurch = this.add.image(2120, 532, 'crate').setScale(.9);
@@ -150,16 +162,21 @@ export class EthanLevel extends Phaser.Scene
         this.map = new TileMap(this, levelChurch, 32, 32, 'sand');
         this.player.setPosition(1400, 525);
 
+        // Sets a new nextLevelGoal with a new function for this area.
         this.nextLevelGoal = new Area(this, 'signPost', 3200, 525, 525, 410).setScale(.25);
         this.nextLevelGoal.whenTouched(this.player, () => {
             this.switchToTown();
         });
 
+        // Spawning in new enemies.
+        // Usage - 
+        // scene.enemyName = new EnemyType(scene, x-pos, y-pos)
         this.priest1 = new Priest(this, 1600, 525);
         this.priest2 = new Priest(this, 1700, 525);
         this.priest3 = new Priest(this, 1800, 525);
         this.priest4 = new Priest(this, 1900, 525);
 
+        // This creates and displays the dialog boxes seen at the top of the screen at certain points in the level.
         let dialogTree = new DialogTree(this, 600, 100);
         let sequence0 = dialogTree.addSequence();
         let sequence1 = dialogTree.addSequence();
@@ -168,6 +185,7 @@ export class EthanLevel extends Phaser.Scene
         dialogTree.addDialog(sequence0, "There are some priests outside who seem to be saying something to you.");
         dialogTree.addDialog(sequence0, "'Do you have a moment to speak about our lord and saviour Mr. Hotsauce?' -The Priests", this.player,
         [
+            // Depending on answer here, the player may or may not receive a power up. Yes for powerup No for no powerup. :)
             ["Of course!", () => {
                 dialogTree.changeSequence(1);  
                 this.priest1.death();
@@ -191,7 +209,7 @@ export class EthanLevel extends Phaser.Scene
     {
         this.map.deleteAllPlatforms();
 
-        // Removing images & enemies used in previous tile map
+        // Removing images, enemies, and nextLevelGoal used in previous tile map
         this.nextLevelGoal.destroy();
         this.church.destroy();
         this.deadTreeChurch.destroy();
@@ -237,7 +255,10 @@ export class EthanLevel extends Phaser.Scene
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ];
 
-        // Adding new images for the background and scene
+        
+        // Adding images to the background.
+        // Usage - 
+        // scene.name = scene.add.image(x-pos, y-pos, 'imageName');
         this.add.image(875, 550, 'fence').setScale(.2);
         this.add.image(940, 550, 'fence').setScale(.2);
         this.add.image(1005, 550, 'fence').setScale(.2);
@@ -267,18 +288,24 @@ export class EthanLevel extends Phaser.Scene
         this.add.image(1620, 525, 'house');
         this.add.image(1645, 525, 'house');
 
+        // Changes the tiles on screen to give the effect of being in a new area without having to create another level.
         this.map = new TileMap(this, levelTown, 32, 32, 'sand');
-        this.player.setPosition(150, 525);
+        this.player.setPosition(150, 525); // Moves the player to the beginning of the tilemap
 
+        // Sets a new nextLevelGoal with a new function for this area.
         this.nextLevelGoal = new Area(this, 'signPost', 3200, 525, 510, 400).setScale(.25);
         this.nextLevelGoal.whenTouched(this.player, () => {
             this.scene.start('level-5');
         });
 
+        // Spawning in new enemies.
+        // Usage - 
+        // scene.enemyName = new EnemyType(scene, x-pos, y-pos)
         this.basicEnemy1 = new Enemy(this, 2800, 525);
         this.basicEnemy2 = new Enemy(this, 2600, 525);
         this.basicEnemy2 = new Enemy(this, 2650, 525);
 
+        // This creates and displays the dialog boxes seen at the top of the screen at certain points in the level.
         let dialogTree = new DialogTree(this, 600, 100);
         let sequence0 = dialogTree.addSequence();
         dialogTree.addDialog(sequence0, "You've made it back to town.", this.player);
