91adbeff7b417c5dc22e644caf4f544a2a7412f8
ETRulz <34170237+ETRulz@users.noreply.github.com>
2019-11-14 20:25:49 -0600

    pause button working
    trying to implement option to click escape to pause and click the button to pause.

--- src/levels/AlexLevel.js
+++ src/levels/AlexLevel.js
@@ -17,6 +17,7 @@ export class AlexLevel extends Phaser.Scene
 
     create()
     {
+        let scale = this.PhaserGame.scale;
         //Creating Level using an Array + Tile Map
         //1 is for block/tile; 0 is for empty space
         //25 wide by 19 long
@@ -67,6 +68,55 @@ export class AlexLevel extends Phaser.Scene
         this.basicEnemy6 = new Enemy(this, 2240, 200);
         this.basicEnemy7 = new Enemy(this, 2272, 200);
         this.basicEnemy8 = new Enemy(this, 2304, 200);
+
+        var paused = false;
+        // positioning of this image is broken somehow only in this level. dont know how to fix -Ethan
+        this.pauseScreen = this.add.sprite(600 * scale, 300 * scale, 'death').setDisplaySize(1200 * scale, 600 * scale).setVisible(false); 
+        this.pauseBtn = this.add.sprite(1150 * scale, 45 * scale, 'pauseBtn').setScale(2.25 * scale).setInteractive().setScrollFactor(0,0);
+        this.unPauseBtn = this.add.sprite(600 * scale, 250 * scale, 'unpauseBtn').setScale(5 * scale).setVisible(false).setScrollFactor(0,0);
+        this.pauseBtn.on('pointerdown', (event) => {
+            if(paused == false){
+                this.player.gun.setVisible(false);
+                this.player.stageMode();
+                this.player.setVisible(false);
+                for(var i = 0; i < this.enemies.list.length; i++) {
+                    this.enemies.list[i].stageMode();
+                    this.enemies.list[i].setVisible(false);
+                }
+                paused = true;
+            }
+            this.pauseScreen.setVisible(true).setAlpha(50);
+            this.pauseBtn.setVisible(false).setInteractive(false);
+            this.unPauseBtn.setVisible(true).setInteractive();
+        });
+        this.unPauseBtn.on('pointerdown', (event) => {
+            if(paused){
+                this.player.gun.setVisible(true);
+                this.player.playMode();
+                this.player.setVisible(true);
+                for(var i = 0; i < this.enemies.list.length; i++) {
+                    this.enemies.list[i].playMode(true);
+                    this.enemies.list[i].setVisible(true);
+                }
+                paused = false;
+            }
+            this.pauseScreen.setVisible(false)
+            this.pauseBtn.setVisible(true).setInteractive(true);
+            this.unPauseBtn.setVisible(false).setInteractive(false);
+        });
+        // Functions to tint the buttons on hover to look nice. :)
+        this.pauseBtn.on('pointerover', function (event) {
+            this.setTint(616161);
+        });
+        this.pauseBtn.on('pointerout', function (event) {
+            this.clearTint();
+        });
+        this.unPauseBtn.on('pointerover', function (event) {
+            this.setTint(616161);
+        });
+        this.unPauseBtn.on('pointerout', function (event) {
+            this.clearTint();
+        });
 
         let dialogTree1 = new DialogTree(this, 600, 100);
         let sequence1 = dialogTree1.addSequence();
